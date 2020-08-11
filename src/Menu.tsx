b5cd3b0ccaf63b90fd132a42125773f08fef2147
Marcin Kwiatkowski <m.kwiatkowski@int.pl>
2020-08-12 00:04:56 +0200

    TS error ! button instead of input

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,12 +1,15 @@
 import React, { useCallback } from "react";
 
 const Menu: React.FC = () => {
-
-  const handleClick = useCallback((e: string) => {
+  const handleClick = useCallback((e: React.MouseEvent) => {
     console.log(e);
   }, []);
-  
-  return <input onClick={handleClick}>TS click</input>;
+
+  return (
+    <div>
+      <button onClick={handleClick}>TS click</button>
+    </div>
+  );
 };
 
 export default Menu;
