a6b28bcb75590550cb0a1682fee83a759bf2afb8
Marcin Kwiatkowski <m.kwiatkowski@int.pl>
2020-08-11 23:43:54 +0200

    rm -rf + hash in js name

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "script.js",
+    filename: "script.[hash].js",
   },
   resolve: {
     extensions: [".js", ".jsx"],
